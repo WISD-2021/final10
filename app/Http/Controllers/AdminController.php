82a0283e5bc2b91df6e0fda9a5a18c42fcf4f4aa
yi xuan <3a832061@gm.student.ncut.edu.tw>
2022-01-10 23:56:29 +0800

    顯示管理介面

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Admin;
 use App\Http\Requests\StoreAdminRequest;
 use App\Http\Requests\UpdateAdminRequest;
-
 class AdminController extends Controller
 {
     /**
@@ -15,7 +14,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin.index');
+        return view('admin.admindex');
     }
 
     /**
--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminProductController extends Controller
 {
-    //
+    public function show(){
+        $products=Product::orderBy('id', 'ASC')->get();
+        $data=['products'=>$products];
+        return view('admin.product', $data);
+    }
 }
